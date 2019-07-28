af745a4b1e710da2c95cd0f3a7f8d6699a31a784
agam indra prasetya <agamindra0@gmail.com>
2019-07-28 18:28:40 +0700

    features/react-2

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import AppHeader from '../common/AppHeader';
+
+class Transaction extends Component {
+    constructor(props){
+        super(props);
+
+
+        
+    }
+    render(){
+        return(
+            <div>
+                <AppHeader />
+            </div>
+        )
+    }
+
+} export default Transaction;
